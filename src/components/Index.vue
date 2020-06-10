437b0d1e67f0734506f8b2693cc4b0fbddf3a4e8
wnf168 <327634465@qq.com>
2020-06-10 09:44:52 +0800

    小模块处理

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -4,8 +4,8 @@
 
 <script>
 import * as THREE from "three";
-import Stats from "../../static/lib/stats"
-import TWEEN from '@tweenjs/tween.js'
+import Stats from "../../static/lib/stats";
+import TWEEN from "@tweenjs/tween.js";
 export default {
   name: "Index",
   data() {
@@ -14,60 +14,86 @@ export default {
       scene: null,
       renderer: null,
       mesh: null,
-      state:null,
-      widths:document.body.clientWidth,
-      heights:document.body.clientHeight
+      state: null,
+      app: null,
+      widths: document.body.clientWidth,
+      heights: document.body.clientHeight
     };
   },
   methods: {
-    init(){
-        let app = document.querySelector("#container");
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(45,this.widths/this.heights,0.1,1000);
-        // this.camera.position.x = 0;
-        // this.camera.position.y = 0;
-        // this.camera.position.z = 600;
-        // this.camera.up.x = 0;
-        // this.camera.up.y = 1;
-        this.camera.position.z = 1;
-        // this.camera.lookAt({
-        //   x:0,
-        //   y:0,
-        //   z:0
-        // })
-        this.renderer = new THREE.WebGLRenderer({
-            antialias:false
-        });
-        this.renderer.setSize(this.widths,this.heights);
-        this.renderer.setClearColor('#87ceeb')
-        app.appendChild(this.renderer.domElement);
-
-        var geometry = new THREE.BoxGeometry(0.2,0.2,0.2);
-        var material = new THREE.MeshBasicMaterial({color:'#ff0000'});
-        this.mesh = new THREE.Mesh(geometry,material);
-        // this.mesh.position = new THREE.Vector3(0,0,0);
-        this.scene.add(this.mesh);
-        this.state = new Stats();
-        this.state.domElement.style.position = "absolute";
-        this.state.domElement.style.left = "0px";
-        this.state.domElement.style.top = "0px";
-        app.appendChild(this.state.domElement)
-
-        
-        this.animate();
-        this.tween();
+    //场景
+    _scene() {
+      this.app = document.querySelector("#container");
+      this.scene = new THREE.Scene();
     },
-    tween(){
-       new TWEEN.Tween(this.camera.position).to({x:1.8},1000).repeat(Infinity).start();
+    //相机
+    _camera() {
+      this.camera = new THREE.PerspectiveCamera(
+        45,
+        this.widths / this.heights,
+        0.1,
+        1000
+      );
+      // this.camera.position.x = 0;
+      // this.camera.position.y = 0;
+      // this.camera.position.z = 600;
+      // this.camera.up.x = 0;
+      // this.camera.up.y = 1;
+      this.camera.position.z = 1;
+      // this.camera.lookAt({
+      //   x:0,
+      //   y:0,
+      //   z:0
+      // })
     },
-    animate(){
-        this.state.begin();
-        requestAnimationFrame(this.animate);
-        //this.mesh.rotation.y += 0.01;
-        //console.log(this.mesh.position.x)
-        this.renderer.render(this.scene,this.camera);
-        TWEEN.update();
-        this.state.end();
+    //渲染器
+    _renderer() {
+      this.renderer = new THREE.WebGLRenderer({
+        antialias: false
+      });
+      this.renderer.setSize(this.widths, this.heights);
+      this.renderer.setClearColor("#87ceeb");
+      this.app.appendChild(this.renderer.domElement);
+    },
+    //几何体
+    _mesh() {
+      var geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+      var material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+      this.mesh = new THREE.Mesh(geometry, material);
+      // this.mesh.position = new THREE.Vector3(0,0,0);
+    },
+    //状态
+    _state() {
+      this.scene.add(this.mesh);
+      this.state = new Stats();
+      this.state.domElement.style.position = "absolute";
+      this.state.domElement.style.left = "0px";
+      this.state.domElement.style.top = "0px";
+      app.appendChild(this.state.domElement);
+    },
+    init() {
+      this._scene();
+      this._camera();
+      this._renderer();
+      this._mesh();
+      this._state();
+      this.animate();
+      this.tween();
+    },
+    tween() {
+      new TWEEN.Tween(this.camera.position)
+        .to({ x: 1.8 }, 1000)
+        .repeat(Infinity)
+        .start();
+    },
+    animate() {
+      this.state.begin();
+      requestAnimationFrame(this.animate);
+      //this.mesh.rotation.y += 0.01;
+      //console.log(this.mesh.position.x)
+      this.renderer.render(this.scene, this.camera);
+      TWEEN.update();
+      this.state.end();
     }
   },
   mounted() {
